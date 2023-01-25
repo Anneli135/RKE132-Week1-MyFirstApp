73fc92ba60cf03b435ca09588ec41a550206b7d6
Anne Li <anneli.harginen@tktk.ee>
2023-01-25 20:12:30 +0200

    creat new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// This is a comment to my code
+
+
+//rakendus küsib kasuatajal sisetada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello, " + userName + "!"); //Output
+
+//string interpolation
+
+Console.WriteLine($"Hello, {userName}!"); //Output
